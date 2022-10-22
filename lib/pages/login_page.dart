fc549826a285473048b5b2015a5b2872d59750e8
Parth Shah <parthshah51999@gmail.com>
2022-10-22 15:33:32 +0530

    add ripple effect on click and form and validations for login

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -10,87 +10,105 @@ class LoginPage extends StatefulWidget {
 
 class _LoginPageState extends State<LoginPage> {
   bool loginClicked = false;
+  final _formKey = GlobalKey<FormState>();
+
+  navigateToHome(BuildContext context) async {
+    if (_formKey.currentState!.validate()) {
+      setState(() {
+        loginClicked = true;
+      });
+
+      await Future.delayed(Duration(seconds: 1));
+      await Navigator.pushNamed(context, AppRoutes.home);
+
+      setState(() {
+        loginClicked = false;
+      });
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Material(
         color: Colors.white,
         child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Image.asset(
-                "assets/images/login.png",
-                fit: BoxFit.cover,
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Text(
-                "Welcome",
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 28),
-              ),
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: 16, horizontal: 32),
-                child: Column(
-                  children: [
-                    TextFormField(
-                      decoration: InputDecoration(
-                          hintText: "Enter Username...", labelText: "Username"),
-                    ),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    TextFormField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                          hintText: "Enter Password...", labelText: "Password"),
-                    )
-                  ],
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 20,
                 ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              InkWell(
-                onTap: () async {
-                  setState(() {
-                    loginClicked = true;
-                  });
-
-                  await Future.delayed(Duration(seconds: 1));
-                  Navigator.pushNamed(context, AppRoutes.home);
-                },
-                child: AnimatedContainer(
-                  duration: Duration(seconds: 1),
-                  height: 40,
-                  width: loginClicked ? 60 : 120,
-                  alignment: Alignment.center,
-                  child: loginClicked
-                      ? Icon(
-                          Icons.done,
-                          color: Colors.white,
-                        )
-                      : Text(
-                          "Login",
-                          style: TextStyle(
+                Image.asset(
+                  "assets/images/welcome.png",
+                  fit: BoxFit.cover,
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 16, horizontal: 32),
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        decoration: InputDecoration(
+                            hintText: "Enter Username...",
+                            labelText: "Username"),
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "Username cannot be empty";
+                          }
+                          return null;
+                        },
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      TextFormField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                            hintText: "Enter Password...",
+                            labelText: "Password"),
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "Password cannot be empty";
+                          }
+                          return null;
+                        },
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Material(
+                  color: Colors.deepPurple,
+                  borderRadius: BorderRadius.circular(loginClicked ? 40 : 8),
+                  child: InkWell(
+                    onTap: () => navigateToHome(context),
+                    child: AnimatedContainer(
+                      duration: Duration(seconds: 1),
+                      height: 50,
+                      width: loginClicked ? 50 : 120,
+                      alignment: Alignment.center,
+                      child: loginClicked
+                          ? Icon(
+                              Icons.done,
                               color: Colors.white,
-                              fontSize: 14,
-                              fontWeight: FontWeight.bold),
-                        ),
-                  decoration: BoxDecoration(
-                      color: Colors.deepPurple,
-                      borderRadius:
-                          BorderRadius.circular(loginClicked ? 40 : 8)),
-                ),
-              )
-              // ElevatedButton(
-              //   onPressed: () {
-              //     Navigator.pushNamed(context, AppRoutes.home);
-              //   },
-              //   child: Text("Login"),
-              //   style: TextButton.styleFrom(minimumSize: Size(120, 40)),
-              // )
-            ],
+                            )
+                          : Text(
+                              "Login",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ));
   }
